d82765b2d0c45c7180c112a536c6d685a5db350d
Colin Bloom <colinjbloom113@gmail.com>
2017-05-23 15:20:13 -0700

    butterknife

--- app/src/main/java/com/example/guest/madlibs/MadLibActivity.java
+++ app/src/main/java/com/example/guest/madlibs/MadLibActivity.java
@@ -1,13 +1,36 @@
 package com.example.guest.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MadLibActivity extends AppCompatActivity {
+    private TextView mResult;
+
+    public String getResult(String v1, String v2, String a, String n, String p) {
+        String result = p + " " + v1 + " from the " + a + " " + n + " and then will " + v2 + " with joy.";
+        return result;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mad_lib);
+
+        mResult = (TextView) findViewById(R.id.result);
+
+        Intent intent = getIntent();
+        String verb1 = intent.getStringExtra("verb1");
+        String verb2 = intent.getStringExtra("verb2");
+        String adjective = intent.getStringExtra("adjective");
+        String noun = intent.getStringExtra("noun");
+        String properNoun1 = intent.getStringExtra("properNoun1");
+        mResult.setText(getResult(verb1, verb2, adjective, noun, properNoun1));
+
+
+
+
     }
 }
--- app/src/main/java/com/example/guest/madlibs/MainActivity.java
+++ app/src/main/java/com/example/guest/madlibs/MainActivity.java
@@ -7,36 +7,28 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mMadLib;
     public static final String TAG = MainActivity.class.getSimpleName();
-    private EditText mVerb1;
-    private EditText mVerb2;
-    private EditText mAdjective;
-    private EditText mNoun;
-    private EditText mProperNoun;
 
-
-//
-//    @Bind (R.id.titleTextView) TextView mTitleTextView;
-//    @Bind (R.id.adjective) TextView mAdjective;
-//    @Bind (R.id.verb1) TextView mVerb1;
-//    @Bind (R.id.verb2) TextView mVerb2;
-//    @Bind (R.id.noun) TextView mNoun;
-//    @Bind (R.id.properNoun1) TextView mProperNoun1;
+    @Bind(R.id.titleTextView) TextView mTitleTextView;
+    @Bind (R.id.adjective) EditText mAdjective;
+    @Bind (R.id.verb1) EditText mVerb1;
+    @Bind (R.id.verb2) EditText mVerb2;
+    @Bind (R.id.noun) EditText mNoun;
+    @Bind (R.id.properNoun1) EditText mProperNoun1;
+    @Bind (R.id.madLib) Button mMadLib;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mMadLib = (Button) findViewById(R.id.madLib);
-        mVerb1 = (EditText) findViewById(R.id.verb1);
-        mVerb2 = (EditText) findViewById(R.id.verb2);
-        mAdjective = (EditText) findViewById(R.id.adjective);
-        mNoun = (EditText) findViewById(R.id.noun);
-        mProperNoun = (EditText) findViewById(R.id.properNoun1);
 
         mMadLib.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +37,13 @@ public class MainActivity extends AppCompatActivity {
                 String verb2 = mVerb2.getText().toString();
                 String adjective = mAdjective.getText().toString();
                 String noun = mNoun.getText().toString();
-                String properNoun = mVerb1.getText().toString();
+                String properNoun1 = mProperNoun1.getText().toString();
                 Intent intent = new Intent(MainActivity.this, MadLibActivity.class);
+                intent.putExtra("verb1", verb1);
+                intent.putExtra("verb2", verb2);
+                intent.putExtra("adjective", adjective);
+                intent.putExtra("noun", noun);
+                intent.putExtra("properNoun1", properNoun1);
                 startActivity(intent);
             }
         });
